d7622a10fb60cd59f166d782fa63b5ea7000e0f3
medeirosJose <joseeduardomj@gmail.com>
2024-01-02 15:30:24 -0300

    teste

---
+++ Next.js/next-challenge/src/components/header/Header.jsx
@@ -0,0 +1,63 @@
+import React from "react";
+import styles from "./styles.module.css";
+import Image from "next/image";
+import Link from "next/link";
+
+const aviso = (text) => {
+  alert(text);
+};
+
+export const Header = ({ icon = "icon.svg" }) => {
+  const handleButtonClick = (message) => {
+    aviso(message);
+  };
+
+  return (
+    <div className={styles.header}>
+      <div className={styles.headerItems}>
+        <div className={styles.group}>
+          <div className={styles.logoContainer}>
+            <p className={styles.JOJOS}>
+              <span className={styles.span}>JOJOS</span>
+            </p>
+            <Image src="/logo.svg" alt="Icon" width={50} height={50} />
+          </div>
+          <div className={styles.menu}>
+            <Link href="">
+              <button
+                className={styles.menuItem}
+                onClick={() => handleButtonClick("Deve mover para baixo")}
+              >
+                Jogos
+              </button>
+            </Link>
+            <Link href="">
+              <button
+                className={styles.menuItem}
+                onClick={() => handleButtonClick("Deve mover para baixo")}
+              >
+                Sobre
+              </button>
+            </Link>
+            <Link href="/blog">
+              <button
+                className={styles.menuItem}
+                onClick={() => handleButtonClick("Página Própria")}
+              >
+                Blog
+              </button>
+            </Link>
+            <Link href="">
+              <button
+                className={styles.menuItem}
+                onClick={() => handleButtonClick("Deve mover para baixo")}
+              >
+                Carreira
+              </button>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
