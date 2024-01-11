12d0a530937dba510152e25b64cfb80ca11cd703
medeirosJose <joseeduardomj@gmail.com>
2024-01-10 22:10:35 -0300

    trying to set new favicon

--- Next.js/next-challenge/src/pages/_app.js
+++ Next.js/next-challenge/src/pages/_app.js
@@ -1,7 +1,15 @@
 import "@/styles/globals.css";
 import "@fontsource/press-start-2p";
 import "@fontsource/montserrat";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <link rel="shortcut icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />;
+    </>
+  );
 }
--- Next.js/next-challenge/src/pages/jobForm/index.jsx
+++ Next.js/next-challenge/src/pages/jobForm/index.jsx
@@ -1,7 +1,6 @@
 import { Header } from "../../components/header/Header";
 import { Footer } from "@/components/footer/Footer";
 import { Form } from "@/components/form/Form";
-
 export const JobForm = () => {
   return (
     <>
